22166603ec5a9f80a7fcd446e5d3622c9deee696
Moises Saez <101760304+Msaez9@users.noreply.github.com>
2023-05-26 14:32:26 -0400

    Add files via upload

---
+++ MusicPro2/market-shop/js/products.js
@@ -0,0 +1,50 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "Sire M7-6 Transparent Black (2nd Gen)",
+    precio: 1998.29 ,
+    img:
+      "https://cdn.shopify.com/s/files/1/0025/5511/7612/products/slapstore_sire_m7_6_transparent_black_2_1800x1800.png?v=1661285567",
+    cantidad: 1,
+  },
+  {
+    id: 2,
+    nombre: "F10 KIT BK GUITARRA ELECTRICA",
+    precio: 412.02,
+    img:
+      "https://falabella.scene7.com/is/image/Falabella/gsc_120986635_2878519_1?wid=1500&hei=1500&qlt=70",
+    cantidad: 1,
+  },
+  {
+    id: 3,
+    nombre: "Yamaha TRBJP2 John Patitucci Red",
+    precio: 5620.21,
+    img:
+      "https://cdn.shopify.com/s/files/1/0025/5511/7612/products/slapstore_yamaha_trbjp2_john_patitucci_signature_translucent_dark_red_body_full_1x1_b350fa3e-ba56-48d4-8bc1-27008c45237d_1800x1800.png?v=1663001083",
+    cantidad: 1,
+  },
+  {
+    id: 4,
+    nombre: "TELSTAR SET BATERIA VOX VOX",
+    precio: 2330.9,
+    img:
+      "https://falabella.scene7.com/is/image/Falabella/gsc_115843008_1286488_1?wid=1500&hei=1500&qlt=70",
+    cantidad: 1,
+  },
+  {
+    id: 5,
+    nombre: "SG50H4C BK BATERIA STAGESTAR 5 PIEZAS",
+    precio: 1024.17,
+    img:
+      "https://falabella.scene7.com/is/image/Falabella/gsc_115854456_1286466_1?wid=1500&hei=1500&qlt=70",
+    cantidad: 1,
+  },
+  {
+    id: 6,
+    nombre: "Batería Electrónica Nitro Mesh Kit",
+    precio: 874.39,
+    img:
+      "https://images.lider.cl/wmtcl?source=url[file:/productos/1221240a.jpg]&sink",
+    cantidad: 1,
+  },
+];
